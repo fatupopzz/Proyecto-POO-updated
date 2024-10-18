b6c377ed0e4bf19cfbc0fb56d078b43c3644b1ce
Jriv24856 <riv24856@uvg.edu.gt>
2024-10-18 00:56:08 -0600

    se agrego vista despues del login
    :)

--- src/main/java/com/restaurantes/Login/script.js
+++ src/main/java/com/restaurantes/Login/script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const signInButton = document.getElementById('sign-in-btn');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -8,4 +9,17 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});
+
+signInButton.addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Datos de login quemados
+    if (email === 'riv24856@uvg.edu.gt' && password === '24856') {
+        // Redirigir a la página de restaurantes
+        window.location.href = './restaurantes.html';
+    } else {
+        alert('Correo o contraseña incorrectos');
+    }
 });
